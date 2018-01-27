3c8ed212f815075f98ce0ead4f9adb01acf9626d
kyChu <hello.kychu@gmail.com>
2018-01-27 15:50:17 +0800

    modify pwm output pin

--- TESTS/TARGET_RDA/pwm/main.cpp
+++ TESTS/TARGET_RDA/pwm/main.cpp
@@ -1,11 +1,11 @@
 #include "mbed.h"
 #include "rtos.h"
 
-PwmOut pwm0(PB_8);
-PwmOut pwm1(PC_1);
-PwmOut pwm2(PB_0);
-PwmOut pwm3(PA_1);
-PwmOut pwm4(PB_3);
+PwmOut pwm0(PD_0);
+PwmOut pwm1(PD_1);
+PwmOut pwm2(PD_2);
+PwmOut pwm3(PD_3);
+//PwmOut pwm4(PB_3);
 
 int main() {
     pwm0.period_ms(4.0f);
@@ -16,8 +16,8 @@ int main() {
     pwm2.write(0.75f);
     pwm3.period_ms(8.0f);
     pwm3.write(0.25f);
-    pwm4.period_ms(8.0f);
-    pwm4.write(0.50f);
+//    pwm4.period_ms(8.0f);
+//    pwm4.write(0.50f);
 
     while(true) {
         printf("duty=%f\r\n", pwm0.read());
